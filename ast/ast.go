470533d41f552593fe12ac20e81eda1277917f10
Tarek El-Masri <tarek.elmasri201@gmail.com>
2024-05-21 23:42:07 +0300

    feat: working on ast and parser

--- ast/ast.go
+++ ast/ast.go
@@ -1,50 +1,105 @@
 package ast
 
-import "github.com/tarek-elmasri/compiler/lexer"
+import (
+	"bytes"
+	"github.com/tarek-elmasri/compiler/lexer"
+)
 
 type Node interface {
-	tokenLiteral() string
+	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
-	statementLiteral() string
+	Node
+	statementNode()
 }
 
 type Expression interface {
-	expressionLiteral() string
+	Node
+	expressionNode()
 }
 
 type Program struct {
 	Statements []Statement
 }
 
-type StatementNode struct {
-	Identifier *lexer.Token
-	Name       string
-	Value      Expression
-}
-
-type ExpressionNode struct {
-	Identifier *lexer.Token
-	Name       string
-	Value      Statement
-}
-
-func (s *StatementNode) statementLiteral() string {
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
 	return ""
 }
 
-func (s *StatementNode) tokenLiteral() string {
-	return s.Identifier.Literal
-}
-
-func (e *ExpressionNode) expresseionLiteral() string { return "" }
-func (e *ExpressionNode) tokenLiteral() string       { return e.Identifier.Literal }
-
 func NewProgramAST() *Program {
-	return &Program{Statements: []Statement{}}
+	return &Program{
+		Statements: []Statement{},
+	}
 }
 
-func (p *Program) Add(s Statement) {
-	p.Statements = append(p.Statements, s)
+type Identifier struct {
+	Token lexer.Token
+	Value string
+}
+
+type LetStatement struct {
+	Token lexer.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+func (ls *LetStatement) String() string {
+	out := bytes.NewBuffer([]byte{})
+	out.WriteString(ls.TokenLiteral())
+	out.WriteString(" ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(lexer.ASSIGN)
+	out.WriteString(ls.Value.String())
+	return out.String()
+}
+
+func (id *Identifier) expresseionNode() {}
+func (id *Identifier) TokenLiteral() string {
+	return id.Token.Literal
+}
+func (id *Identifier) String() string {
+	return id.TokenLiteral()
+}
+
+type ReturnStatement struct {
+	Token       lexer.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) String() string {
+	out := bytes.NewBuffer([]byte{})
+	out.WriteString(rs.TokenLiteral())
+	out.WriteString(" ")
+	out.WriteString(rs.ReturnValue.String())
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      lexer.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) String() string {
+	// TODO: return expression string for now!
+	return es.Expression.String()
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,50 +1,113 @@
-// package parser
-//
-// import (
-// 	"fmt"
-//
-// 	"github.com/tarek-elmasri/compiler/ast"
-// 	"github.com/tarek-elmasri/compiler/lexer"
-// )
-//
-// type Parser struct {
-// 	lexer     *lexer.Lexer
-// 	curToken  lexer.Token
-// 	peekToken lexer.Token
-// 	errors    []string
-// }
-//
-// func New(l *lexer.Lexer) *Parser {
-// 	p := Parser{
-// 		lexer:  l,
-// 		errors: []string{},
-// 	}
-//
-// 	p.nextToken()
-// 	return &p
-// }
-//
-// func (p *Parser) nextToken() {
-// 	p.curToken = p.peekToken
-// 	p.peekToken = p.lexer.NextToken()
-// }
-//
-// func (p *Parser) Errors() []string { return p.errors }
-//
-// func (p *Parser) ParseProgram() *ast.Program {
-// 	program := ast.NewProgramAST()
-//
-// 	for p.curToken.TokenType != lexer.EOF {
-// 		switch p.curToken.TokenType {
-// 		case lexer.LET, lexer.CONST, lexer.VAR:
-// 			if p.peekToken.TokenType != lexer.IDENT || p.peekToken.TokenType != lexer.COMMA {
-// 				p.errors = append(p.errors, fmt.Sprintf("unexpected %s.", p.peekToken.Literal))
-// 			}
-//
-// 			s := ast.StatementNode{Identifier: &p.curToken}
-//
-// 		}
-//
-// 		p.nextToken()
-// 	}
-// }
+package parser
+
+import (
+	"fmt"
+
+	"github.com/tarek-elmasri/compiler/ast"
+	"github.com/tarek-elmasri/compiler/lexer"
+)
+
+type Parser struct {
+	lexer     *lexer.Lexer
+	curToken  lexer.Token
+	peekToken lexer.Token
+	errors    []string
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := Parser{
+		lexer:  l,
+		errors: []string{},
+	}
+
+	p.nextToken()
+	p.nextToken()
+	return &p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.lexer.NextToken()
+}
+
+func (p *Parser) Errors() []string { return p.errors }
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := ast.NewProgramAST()
+	for p.curToken.TokenType != lexer.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.TokenType {
+	case lexer.LET:
+		return p.parseLetStatement()
+	case lexer.RETURN:
+		return p.parseReturnStatement()
+	default:
+		return nil
+	}
+
+}
+
+func (p *Parser) addUnexpectedTokenError() {
+	p.errors = append(p.errors, fmt.Sprintf("line: %d:%d - unexpected %s.", p.peekToken.LineNumber, p.peekToken.PositionNumber, p.peekToken.Literal))
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	// TODO: multi assign statement
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(lexer.IDENT) {
+		p.addUnexpectedTokenError()
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+
+	if !p.expectPeek(lexer.ASSIGN) {
+		p.addUnexpectedTokenError()
+		return nil
+	}
+
+	// TODO: read expression value
+
+	return stmt
+}
+
+func (p *Parser) currentTokenIs(tt lexer.TokenType) bool {
+	return p.curToken.TokenType == tt
+}
+
+func (p *Parser) peekTokenIs(tt lexer.TokenType) bool {
+	return p.peekToken.TokenType == tt
+}
+
+// if next token == expected -> move forward
+func (p *Parser) expectPeek(tt lexer.TokenType) bool {
+	if p.peekToken.TokenType == tt {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+	// TODO: read return value
+	return stmt
+}

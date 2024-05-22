b7105b790ec742aa39c23e495f76fa1ef7306c3d
Tarek El-Masri <tarek.elmasri201@gmail.com>
2024-05-22 05:49:00 +0300

    feat: add repl

---
+++ repel/repel.go
@@ -0,0 +1,40 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/tarek-elmasri/compiler/lexer"
+)
+
+const PROPMT = ">> "
+
+type Repl struct {
+	reader io.Reader
+}
+
+func New(r io.Reader) *Repl {
+	return &Repl{
+		reader: r,
+	}
+}
+
+func (r *Repl) Start() {
+	scanner := bufio.NewScanner(r.reader)
+
+	for {
+		fmt.Printf(PROPMT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text() + "\n"
+		l := lexer.New(line)
+		for t := l.NextToken(); t.TokenType != lexer.EOF; t = l.NextToken() {
+			fmt.Printf("%+v\n", t)
+		}
+	}
+
+}
